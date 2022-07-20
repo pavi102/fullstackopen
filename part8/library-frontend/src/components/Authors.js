7841b824da1ee94e6b4c7a23f4ebaa231cadfbd0
pavi102 <pavi1022@gmail.com>
2022-07-19 17:15:11 -0700

    completed exercises 8.8-8.12

---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,40 @@
+import { useQuery } from "@apollo/client";
+import { ALL_AUTHORS } from "../queries";
+import EditAuthor from "./EditAuthor";
+
+const Authors = props => {
+  const result = useQuery(ALL_AUTHORS);
+
+  if (!props.show) {
+    return null;
+  }
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {result.data.allAuthors.map(a => (
+            <tr key={a.id}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <EditAuthor authors={result.data.allAuthors} />
+    </div>
+  );
+};
+
+export default Authors;
---
+++ part8/library-frontend/src/components/EditAuthor.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { EDIT_AUTHOR } from "../queries";
+const EditAuthor = ({ authors }) => {
+  const [name, setName] = useState("");
+  const [born, setBorn] = useState("");
+
+  const [editAuthor] = useMutation(EDIT_AUTHOR);
+  const handleChangeYear = e => {
+    e.preventDefault();
+    editAuthor({ variables: { name, born: Number(born) } });
+    setName("");
+    setBorn("");
+  };
+
+  return (
+    <div>
+      <h1>Set Birth Year</h1>
+      <form onSubmit={handleChangeYear}>
+        name
+        <select value={name} onChange={e => setName(e.target.value)}>
+          {authors.map(author => (
+            <option key={author.id} value={author.name}>
+              {author.name}
+            </option>
+          ))}
+        </select>
+        born
+        <input
+          value={born}
+          type={"number"}
+          onChange={e => setBorn(e.target.value)}
+        />
+        <button onClick={handleChangeYear}>update author</button>
+      </form>
+    </div>
+  );
+};
+export default EditAuthor;
